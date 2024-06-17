ea2c870eb261c050fde0226c7947d80ca5398d43
KikiBerg <153670000+KikiBerg@users.noreply.github.com>
2024-06-17 12:49:56 +0200

    Skapa klassen Motorcycle

---
+++ Ovning5-Garage/Models/Motorcycle.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ovning5_Garage.Models
+{
+    public class Motorcycle : Vehicle
+    {
+        // Unik egenskap för Motorcykel, representerar cylindervolymen
+        public double CylinderVolume { get; set; }
+
+        // Konstruktor för Motorcykel som tar registreringsnummer, färg, antal hjul och cylindervolym som parametrar
+        public Motorcycle(string registrationNumber, string colour, int numOfWheels, double cylinderVolume) : base(registrationNumber, colour, numOfWheels)
+        {
+            CylinderVolume = cylinderVolume; // Tilldelar cylindervolymet till egenskapen CylinderVolume
+        }   
+      
+
+        
+       
+    }
+}
