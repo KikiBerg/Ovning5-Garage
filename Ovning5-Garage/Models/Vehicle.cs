5d9f08b5e705a87471ff2d1b0c7ade241593546b
KikiBerg <153670000+KikiBerg@users.noreply.github.com>
2024-06-17 11:17:31 +0200

    Skapa klassen Vehicle

---
+++ Ovning5-Garage/Models/Vehicle.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ovning5_Garage.Models
+{
+    // Definiera klassen Vehicle som samtliga subklasser ärver ifrån.
+    public class Vehicle
+    {
+        // Deklarera klassens olika egenskaper
+        public string RegistrationNumber { get; set; }
+        public string Colour { get; set; }
+        public int NumOfWheels { get; set; }
+
+        // Konstruktor för att skapa ett nytt fordonobjekt
+        public Vehicle(string registrationNumber, string colour, int numOfWheels) 
+        {
+            // Här tilldelar jag de angivna värdena till fordonets egenskaper
+            RegistrationNumber = registrationNumber;
+            Colour = colour;
+            NumOfWheels = numOfWheels;
+        }
+
+
+    }
+}
