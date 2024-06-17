d3aa931d216c61ffe083acc3b06e9a112197b142
KikiBerg <153670000+KikiBerg@users.noreply.github.com>
2024-06-17 12:37:02 +0200

    Skapa klassen Airplane

---
+++ Ovning5-Garage/Models/Airplane.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ovning5_Garage.Models
+{
+    public class Airplane : Vehicle
+    {
+        // Egenskap unik för flygplan som anger antalet motorer
+        public int NumOfEngines { get; set; }
+
+        // Konstruktor för Airplane-klassen som tar registreringssnummer, färg, antal hjul och antal motorer som parametrar
+        public Airplane(string registrationNumber, string colour, int numOfWheels, int numOfEngines):base(registrationNumber, colour, numOfWheels) 
+        {
+            NumOfEngines = numOfEngines; // Tilldelar det angivna värdet till egenskapen NumOfEngines
+        }
+    }
+}
