c1b10807dbbab82a2cf69de5e76eb32005f68dca
KikiBerg <153670000+KikiBerg@users.noreply.github.com>
2024-06-17 13:10:29 +0200

    Skapa klassen Bus

---
+++ Ovning5-Garage/Models/Bus.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ovning5_Garage.Models
+{
+    public class Bus : Vehicle // Definierar en klass som ärver från Vehicle
+    {
+        //Deklarera unika egenskaper för Bus
+        public int NumOfSeats { get; set; }
+
+        // Konstruktor för bussklassen som tar registreringsnummer, färg, antal hjul och antal sittplatser som parametrar
+        public Bus(string registrationNumber, string colour, int numOfWheels, int numOfSeats) : base(registrationNumber, colour, numOfWheels) // Anropa basklassens konstruktor för att initialisera gemensamma attribut
+        {
+            // Initialisera den unika attributen för Bus
+            NumOfSeats = numOfSeats;
+        }
+    }
+}
