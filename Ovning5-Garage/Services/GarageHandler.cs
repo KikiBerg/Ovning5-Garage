7de23e41ac23dce1a1c63197a457cbc03954ce88
KikiBerg <153670000+KikiBerg@users.noreply.github.com>
2024-06-18 15:36:12 +0200

    Add CreateGarage method

--- Ovning5-Garage/Services/GarageHandler.cs
+++ Ovning5-Garage/Services/GarageHandler.cs
@@ -8,8 +8,26 @@ using System.Collections;
 
 namespace Ovning5_Garage.Services
 {
+
+    /*
+     * Den här klassen fungerar som en mellanhand mellan användargränssnittet och Garage-klassen.
+     * Den erbjuder metoder för att lägga till, ta bort, hitta och lista fordon i garaget. 
+     */
+
     public class GarageHandler
     {
+        private Garage<Vehicle>? garage; // Anslag till en generisk garageskap för fordonstyper
+
+        // Metod för att skapa en ny garage med angiven kapacitet
+        public void CreateGarage(int capacity)
+        {
+            garage = new Garage<Vehicle>(capacity); // Skapar en ny instans av Garage med kapaciteten som parameter
+            Console.WriteLine($"Garage skapad med kapacitet: {capacity}");
+        }
+
+
+
+
 
     }
 }
