90b51acfa6b8c04e7e4f5c485b60621d746063cc
KikiBerg <153670000+KikiBerg@users.noreply.github.com>
2024-06-18 17:14:00 +0200

    Add IVehicle

---
+++ Ovning5-Garage/Interfaces/IVehicle.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Ovning5_Garage.Interfaces
+{
+
+    /* Det här gränssnitt definierar kontraktet för fordon.
+     * Definierar egenskaper som en klass (t.ex., Vehicle och dess subklasser) måste implementera för att representera ett fordon:
+     */
+
+
+    public interface IVehicle
+    {
+        string RegistrationNumber { get; set; } // Denna egenskap representerar det unika registreringsnumret för fordonet.
+        string Colour { get; set; } // Denna egenskap representerar färgen på fordonet.
+        int NumOfWheels { get; set; } // Denna egenskap representerar antalet hjul på fordonet.
+    }
+}
