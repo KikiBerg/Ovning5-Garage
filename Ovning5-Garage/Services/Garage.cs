032ba5e4880341104acc987f1789c5df2e30e8bd
KikiBerg <153670000+KikiBerg@users.noreply.github.com>
2024-06-18 11:12:14 +0200

    Börja definiera Garage klassen.

---
+++ Ovning5-Garage/Services/Garage.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Ovning5_Garage.Models;
+
+namespace Ovning5_Garage.Services
+{
+    public class Garage<T> : where T : Vehicle //Definiera en generisk klass som hanterar objekt av typ Vehicle
+    {
+        private T[] vehicles;   // Array för att lagra fordon
+        private int capacity;   // Kapaciteten på garaget
+        private int count;      // Antalet fordon i garaget
+    }
+
+    
+
+
+}
+
