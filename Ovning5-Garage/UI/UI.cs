f50b186070c4a90824cf8c9773b5a83f998df110
KikiBerg <153670000+KikiBerg@users.noreply.github.com>
2024-06-19 12:40:46 +0200

    UI: define AddVehicle

--- Ovning5-Garage/UI/UI.cs
+++ Ovning5-Garage/UI/UI.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Ovning5_Garage.Models;
+using System.Drawing;
 
 
 namespace Ovning5_Garage.UI
@@ -21,9 +22,44 @@ namespace Ovning5_Garage.UI
         }
 
 
+        // Metod för att lägga till ett nytt fordon
         public void AddVehicle()
         {
-            throw new NotImplementedException();
+            Console.WriteLine("Ange fordontypen (Flygplan, Motorcykel, Bil, Buss, Båt):");
+            string type = Console.ReadLine();
+            
+            Console.WriteLine("Ange registreringsnumret:");
+            string registrationNumber = Console.ReadLine();
+            
+            Console.WriteLine("Ange färgen:");
+            string colour = Console.ReadLine();
+            
+            Console.WriteLine("Ange antalet hjul:");
+            int numOfWheels = int.Parse(Console.ReadLine());
+
+            // Skapar ett nytt fordonobjekt baserat på användarinput
+            Vehicle vehicle = type.ToLower() switch
+            {
+                "flygplan" => new Airplane(registrationNumber, colour, numOfWheels, 2), // Standardantal motorer
+                "motorcykel" => new Motorcycle(registrationNumber, colour, numOfWheels, 1000), // Standard cylindervolym
+                "bil" => new Car(registrationNumber, colour, numOfWheels, "Bensin"), // Standard bränsletyp
+                "buss" => new Bus(registrationNumber, colour, numOfWheels, 50), // Standardantal platser
+                "båt" => new Boat(registrationNumber, colour, numOfWheels, 30), // Standard längd
+                _ => null
+            };
+
+            // Jag kontrollerar om objektet är giltigt och lägger till det i handler
+            if (vehicle != null)
+            {
+                _handler.AddVehicle(vehicle);
+            }
+            else
+            {
+                Console.WriteLine("Ogiltig fordonstyp");
+            }
+
+
+
         }
 
         public void FindVehicle()
